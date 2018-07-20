ae78d9e64d3e9447938f266fdacb0c639dd1ae72
Erik Olofsson <erik.olofsson@favro.com>
2018-07-20 16:11:56 +0200

    [Malterlib/Cloud/CloudClient] Change default timeout to 120 seconds

--- Apps/CloudClient/Source/Malterlib_Cloud_App_CloudClient_CommandLine.cpp
+++ Apps/CloudClient/Source/Malterlib_Cloud_App_CloudClient_CommandLine.cpp
@@ -27,7 +27,7 @@ namespace NMib::NCloud::NCloudClient
 					"Timeout?"_= 
 					{
 						"Names"_= {"--timeout"}
-						,"Default"_= 10.0
+						,"Default"_= 120.0
 						, "Description"_= "The number of seconds to wait for remote servers to reply to commands"
 					}
 				}
