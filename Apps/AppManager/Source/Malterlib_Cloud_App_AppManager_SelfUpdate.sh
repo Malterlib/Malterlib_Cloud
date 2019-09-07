61bb5a54f871930a94017757fddc23aca52f2ee5
Erik Olofsson <erik@olofsson.info>
2019-09-07 18:36:50 +0200

    [Malterlib/Cloud] Add logs to self update script

--- Apps/AppManager/Source/Malterlib_Cloud_App_AppManager_SelfUpdate.sh
+++ Apps/AppManager/Source/Malterlib_Cloud_App_AppManager_SelfUpdate.sh
@@ -1,16 +1,35 @@
 R"-----(#!/bin/bash
 
+set -e
+
+LogFile="$PWD/Log/AppManagerSelfUpdate.log"
+function Log()
+{
+	echo "`date +"%Y-%m-%d %H:%M:%S"`" $$ "$@" >> "$LogFile"
+}
+
+function ErrorReport()
+{
+	Log "Error on line $(caller)"
+}
+
+trap ErrorReport ERR
+
 if [[ "$3" != "Recursive" ]]; then
+	Log "Launching recursive"
 	set -m
 	"${BASH_SOURCE[0]}" "$1" "$2" Recursive </dev/null &>/dev/null &
 	disown
 	exit 0
 fi
 
+Log "Launched recursive"
+
 SysName=$(uname -s)
 
 function WaitForPidToExit()
 {
+	Log "Waiting for AppManager to exit"
 	if [[ $SysName ==  Darwin* ]] ; then
 		lsof -p $1 +r 1 &>/dev/null
 	elif [[ $SysName ==  Linux* ]] ; then
@@ -20,22 +39,28 @@ function WaitForPidToExit()
             sleep 0.1
         done
 	fi
+	Log "AppMangare exited"
 }
 
 if [[ "$2" == "DaemonStandalone" ]]; then
 	WaitForPidToExit $AppManagerPID
-	"$1" --daemon-run-standalane &
+	Log "Launching new standalone executable"
+	"$1" --daemon-run-standalone >> "$LogFile" 2>&1 &
 	disown
 elif [[ "$2" == "DaemonDebug" ]]; then
 	WaitForPidToExit $AppManagerPID
-	"$1" --daemon-run-debug &
+	Log "Launching new daemon debug executable"
+	"$1" --daemon-run-debug >> "$LogFile" 2>&1 &
 	disown
 elif [[ "$2" == "Daemon" ]]; then
 	WaitForPidToExit $PPID
-	"$1" --daemon-restart
+	Log "Launching new daemon executable"
+	"$1" --daemon-restart >> "$LogFile" 2>&1
 else
-	echo "Unknow self update type"
+	Log "Unknow self update type: $2"
 	exit 1
 fi
+
+Log "Successfully launched"
 
 )-----"
