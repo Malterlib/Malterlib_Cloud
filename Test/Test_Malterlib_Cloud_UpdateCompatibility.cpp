28d0b8b05d969a4107366c796398db830188b635
Erik Olofsson <erik@olofsson.info>
2026-08-28 12:39:23 +0200

    Pass the test color setting to launched apps and log forwarders
    The app manager harness and the update compatibility test launched
    apps and log forwarders with the default color setting, so a run
    without color still produced colored logs. Both now forward the
    effective test ansi flags: --no-color to launched apps and the color
    choice to the log forwarder.

--- Test/Test_Malterlib_Cloud_UpdateCompatibility.cpp
+++ Test/Test_Malterlib_Cloud_UpdateCompatibility.cpp
@@ -354,7 +354,12 @@ struct CUpdateCompatibility_Tests : public NMib::NTest::CTest
 			}
 		}
 
-		TCActor<CDistributedAppLogForwarder> LogForwarder{fg_Construct(RootDirectory), "Log Forwarder Actor"};
+		TCActor<CDistributedAppLogForwarder> LogForwarder
+			{
+				fg_Construct(RootDirectory, !!(fg_TestAnsiEncodingFlags() & NCommandLine::EAnsiEncodingFlag_Color))
+				, "Log Forwarder Actor"
+			}
+		;
 
 		auto CleanupLogForwarder = g_OnScopeExit / [&]
 			{
@@ -1421,7 +1426,12 @@ struct CUpdateCompatibility_Tests : public NMib::NTest::CTest
 			}
 		}
 
-		TCActor<CDistributedAppLogForwarder> LogForwarder{fg_Construct(RootDirectory), "Log Forwarder Actor"};
+		TCActor<CDistributedAppLogForwarder> LogForwarder
+			{
+				fg_Construct(RootDirectory, !!(fg_TestAnsiEncodingFlags() & NCommandLine::EAnsiEncodingFlag_Color))
+				, "Log Forwarder Actor"
+			}
+		;
 
 		auto CleanupLogForwarder = g_OnScopeExit / [&]
 			{
